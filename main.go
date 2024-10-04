b2ddbfd67fbe3e6818f8416da804bf762f2f7cae
Inajara Lopes <inajara-lopes@hotmail.com>
2024-10-04 16:04:52 -0300

    add main.go

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, Go Essentials!")
+}
+
+
+
+// Pasta == folders podem ter multiplos arquivos
+// Pasta pode ter multiplas funções dentro dela (espalhadas em multiplos arquivos)
+// Toda pasta é um pacote
+//Funções pertencem a um pacote
